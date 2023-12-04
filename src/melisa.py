75189ffc9c9f0fd2f0c012a27c61c290c589e510
Daniel Esteban Guzmán <estebantoon@hotmail.com>
2023-12-04 10:18:30 -0300

    Receiving and storing images

--- src/melisa.py
+++ src/melisa.py
@@ -1,14 +1,14 @@
+import os
 import re
 from flask import Flask, request, Response
 import requests
 import telegram
+from datetime import datetime
+import uuid
 
 app = Flask(__name__)
 
-FOLDER_APP = "/home/hsotelo/melisa/telegram/"
-#FOLDER_APP = "/home/hsotelo/melisatg/"
-#FOLDER_APP = "G:\\Me\\Code\\UOC\\TFM\\demeter\\src\\facebook_client\\"
-#FOLDER_APP = "/app/"
+FOLDER_APP = "/var/www/melisa/telegram/"
 FILE_TOKEN = FOLDER_APP + "token.txt"
 FILE_TOKEN_DEMETER = FOLDER_APP + "token_demeter.txt"
 TOKEN = ""
@@ -21,6 +21,42 @@ DEMETER_URL = "https://demeter.aclimate.org/api/v1/query/"
 URL = "https://melisatg.aclimate.org/"
 BOT_USER_NAME = "Melisa_chatbot"
 bot = None
+
+# Función que maneja las imágenes
+def handle_image(update, context):
+    try:
+        # Verifica si el mensaje contiene una foto
+        print("Mensaje de foto recibido")
+        if update.message.photo:
+            # Obtiene el objeto de la foto más grande
+            photo = update.message.photo[-1]
+
+            # Obtiene el identificador único del usuario
+            user_id = update.message.from_user.id
+
+            # Genera un nombre único para la imagen basado en un UUID
+            file_id = str(uuid.uuid4())
+
+            # Obtiene la fecha actual en formato YYYYMMDD
+            date_today = datetime.now().strftime("%Y%m%d")
+
+            # Crea el directorio si no existe
+            user_dir = os.path.join(".//images", date_today, str(user_id))
+            os.makedirs(user_dir, exist_ok=True)
+
+            # Guarda la imagen en el directorio correspondiente
+            file_path = os.path.join(user_dir, f"{file_id}.jpg")
+            photo.get_file().download(file_path)
+
+            # Envia la información a Demeter u realiza otras acciones necesarias
+            # ...
+
+            # Devuelve la misma imagen como respuesta
+            update.message.reply_photo(photo.file_id)
+            print("Imagen guardada exitosamente")
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 @app.route('/')
 def index():
@@ -36,24 +72,28 @@ def set_webhook():
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
-    # retrieve the message in JSON and then transform it to Telegram object
+    # retrieve the message in JSON and then transform it to a Telegram object
     update = telegram.Update.de_json(request.get_json(force=True), bot)
 
     sender_id = str(update.message.chat.id)
     ext_id = str(update.message.message_id)
+
     try:
-        # Telegram understands UTF-8, so encode text for unicode compatibility
-        text = update.message.text.encode('utf-8').decode()
-        #text = update.message.text    
-        text = re.sub(r"\W", "_", text)
-        if text != "/start":
-            json = {"melisa":MELISA_NAME,"token":TOKEN_DEMETER,"user":sender_id,"chat_id":ext_id,"message":text}
-            r = requests.post(DEMETER_URL, json=json)
-            #url = DEMETER_URL + "?melisa=" + MELISA_NAME + "&token=" + TOKEN_DEMETER + "&user=" + sender_id + "&chat_id=" + ext_id + "&message=" + text
-            #requests.get(url, timeout=1)
-        return Response('ok',200)
-    except Exception:
-        return Response('Bad request',400)
+        if update.message.photo:
+            # Llama a la función para manejar imágenes
+            handle_image(update, None)
+        else:
+            # Procesa mensajes de texto
+            text = update.message.text.encode('utf-8').decode()
+            text = re.sub(r"\W", "_", text)
+            if text != "/start":
+                json_data = {"melisa": MELISA_NAME, "token": TOKEN_DEMETER, "user": sender_id, "chat_id": ext_id, "message": text}
+                r = requests.post(DEMETER_URL, json=json_data)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    return Response('ok', 200)
 
 @app.route("/receptor", methods=['POST'])
 def receptor():
@@ -61,8 +101,9 @@ def receptor():
     token = data['token']
     messages = data['text']
     sender_id = data['user_id']
-    chat_id = data['chat_id'] if 'chat_id' in data is None else None
+    chat_id = data['chat_id'] if 'chat_id' in data else None
     first = True
+
     if token == TOKEN_DEMETER:
         for m in messages:
             if first and chat_id is not None:
@@ -70,17 +111,15 @@ def receptor():
                 first = False
             else:
                 bot.sendMessage(chat_id=sender_id, text=m)
-    return Response('ok',200)
 
+    return Response('ok', 200)
 
 if __name__ == '__main__':
     with open(FILE_TOKEN_DEMETER, "r") as f:
         TOKEN_DEMETER = f.read()
+
     bot = telegram.Bot(token=TOKEN)
 
-    #app.run(threaded=True, port=5000)
+    # app.run(threaded=True, port=5000)
     app.run(host='0.0.0.0', port=5001)
     print("Start server on PORT 5001")
-
-# Run in background
-# nohup python3 melisa.py > melisa.log 2>&1 &
